9d13387c751f2cb1b1a7b2b327b2ebb721cd6a55
Jacob <121278501+JacobZCodes@users.noreply.github.com>
2025-07-28 11:43:18 -0700

    home button + modal

---
+++ script.js
@@ -0,0 +1,29 @@
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+const homeBtn = document.getElementById("homeBtn");
+const defaultInnerModalHTML = `<h3 style="color: #000;">Push "Enter" to progress</h3> <h3 style="color: black">Press the home icon at any time to return to the landing page.</h3>`;
+
+function updateInnerModalHTML(newHTML) {
+    document.getElementById("modal-inner-text").innerHTML = newHTML;
+
+}
+
+function resetInnerModalHTML() {
+    document.getElementById("modal-inner-text").innerHTML = defaultInnerModalHTML;
+}
+
+function openModal() {
+    modal.classList.add("open");
+}
+
+openBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+})
+
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("open");
+    resetInnerModalHTML();
+    
+})
+
