d5c2bef695e373622b2cb2f5ca007c9f9fb4f07a
Jacob <121278501+JacobZCodes@users.noreply.github.com>
2025-08-11 17:05:19 -0700

    cleaned everything

--- Scripts/animations.js
+++ Scripts/animations.js
@@ -2,3 +2,11 @@ function fadeIn(el) {
     el.style.opacity = "0";    
     el.style.animation = "fadeIn 0.6s forwards";
   }
+
+function fadeInAndResetAnimaton(el) {
+    console.log("called");
+    el.style.opacity = "0";
+    el.style.animation = "none";   
+    el.style.animation = "fadeIn 0.6s forwards";
+    
+  }
